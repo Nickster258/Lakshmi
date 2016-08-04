da88c4c068ffe71d9154e26eb8be198398bffdde
Nickster258 <nickster258@gmail.com>
2016-08-04 16:50:13 -0400

    Cleaned imports on IRCBot.java

--- IRCBot.java
+++ IRCBot.java
@@ -1,6 +1,9 @@
-import java.util.Date;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.Socket;
 
 public class IRCBot {
 
