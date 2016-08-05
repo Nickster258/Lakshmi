c2e4840c5f371d61b2d1f8d2c1cf18d7a973f2fb
Nickster258 <nickster258@gmail.com>
2016-08-05 14:31:45 -0400

    Fixed stranged formatting with imports

--- bot.java
+++ bot.java
@@ -1,5 +1,11 @@
-import java.io.BufferedReader; import java.io.BufferedWriter; import java.io.OutputStream; import java.io.InputStream; import java.io.InputStreamReader; import 
-java.io.FileInputStream; import java.io.FileReader; import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.net.HttpURLConnection;
